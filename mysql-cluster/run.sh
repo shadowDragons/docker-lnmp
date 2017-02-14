d2d7e79e1604b364ed5911c2c7cf36f46eddcb3f
俊滨 <shadow_dragon@126.com>
2017-02-14 22:34:43 +0800

    fix mysql

--- mysql-cluster/run.sh
+++ mysql-cluster/run.sh
@@ -13,7 +13,13 @@ chmod 644 /etc/mysql/conf.d/mysqld_charset.cnf
 
 StartMySQL ()
 {
-    /usr/bin/mysqld_safe ${EXTRA_OPTS} > /dev/null 2>&1 &
+    TARGET_UID=$(stat -c "%u" /var/lib/mysql)
+    usermod -o -u $TARGET_UID mysql || true
+    TARGET_GID=$(stat -c "%g" /var/lib/mysql)
+    groupmod -o -g $TARGET_GID mysql || true
+    chown -R mysql:root /var/run/mysqld/
+
+    /usr/bin/mysqld_safe --user=mysql ${EXTRA_OPTS} > /dev/null 2>&1 &
     # Time out in 1 minute
     LOOP_LIMIT=60
     for (( i=0 ; ; i++ )); do
@@ -158,6 +164,8 @@ if [ -n "${REPLICATION_MASTER}" ]; then
     echo "=> Configuring MySQL replication as master (2/2) ..."
     if [ ! -f /replication_set.2 ]; then
         echo "=> Creating a log user ${REPLICATION_USER}:${REPLICATION_PASS}"
+        mysql -uroot -e "delete from mysql.user where user = '${REPLICATION_USER}'"
+		mysql -uroot -e "FLUSH PRIVILEGES"
         mysql -uroot -e "CREATE USER '${REPLICATION_USER}'@'%' IDENTIFIED BY '${REPLICATION_PASS}'"
         mysql -uroot -e "GRANT REPLICATION SLAVE ON *.* TO '${REPLICATION_USER}'@'%'"
         mysql -uroot -e "reset master"
@@ -174,6 +182,7 @@ if [ -n "${REPLICATION_SLAVE}" ]; then
     if [ -n "${MYSQL_PORT_3306_TCP_ADDR}" ] && [ -n "${MYSQL_PORT_3306_TCP_PORT}" ]; then
         if [ ! -f /replication_set.2 ]; then
             echo "=> Setting master connection info on slave"
+            echo "=> '${MYSQL_PORT_3306_TCP_ADDR}' '${MYSQL_PORT_3306_TCP_PORT}'"
             mysql -uroot -e "CHANGE MASTER TO MASTER_HOST='${MYSQL_PORT_3306_TCP_ADDR}',MASTER_USER='${MYSQL_ENV_REPLICATION_USER}',MASTER_PASSWORD='${MYSQL_ENV_REPLICATION_PASS}',MASTER_PORT=${MYSQL_PORT_3306_TCP_PORT}, MASTER_CONNECT_RETRY=30"
             mysql -uroot -e "start slave"
             echo "=> Done!"
